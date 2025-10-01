5c8eff47ddaf8810bdce356d7fe4162a82732eee
Soham Panchal <sohamdesu@gmail.com>
2025-10-01 20:28:23 +0530

    feat: Deploys demo on github pages (#4)
    * feat: Deploys demo on github pages
    
    * fix: Fixes the deployment workflow issue skipping tcs compilation

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
   plugins: [react()],
-})
+  base: mode === 'production' ? '/TPS-Controls/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+}))
